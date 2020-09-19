f7243fdc11df568dbab2cec0df07847694aa4906
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-09-19 23:23:44 +0800

    modify

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 
 from app import app
 
+import pickle
+import gzip
+
+with gzip.open('xgboost-iris.pgz', 'r') as f:
+    xgboostModel = pickle.load(f)
+    print(xgboostModel)
+
+
 @app.route('/')
 def index():
     return 'hello  !'
