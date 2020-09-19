233941e377e9efdc3c8689fcbb1ae0dd1983b34d
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-09-19 21:37:31 +0800

    modify dir

--- app/model.py
+++ app/model.py
@@ -2,13 +2,18 @@ import pickle
 import gzip
 import xgboost as xgb
 
-PROJECT_DIR=''
-# 載入Model
-with gzip.open(PROJECT_DIR+'app/model/xgboost-iris.pgz', 'rb') as f:
-    print(xgb.__version__)
+# PROJECT_DIR=''
+# # 載入Model
+# with gzip.open(PROJECT_DIR+'app/model/xgboost-iris.pgz', 'rb') as f:
+#     print(xgb.__version__)
+#     xgboostModel = pickle.load(f)
+#     print(xgboostModel)
+
+#讀取Model
+with open('app/model/xgboost-iris.pickle', 'rb') as f:
     xgboostModel = pickle.load(f)
-    print(xgboostModel)
-    
+    pred=xgboostModel.predict(np.array([[5.5, 2.4, 3.7, 1. ]]))
+    print(pred)
 
 def test():
     return "This is model.py"
