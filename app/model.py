22af51d1402ce6fc3774790cc342d350100acbe8
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-10-11 10:48:05 +0800

    commit

---
+++ app/model.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+import pickle
+import gzip
+
+# 載入Model
+with gzip.open('app/model/xgboost-iris.pgz', 'r') as f:
+    xgboostModel = pickle.load(f)
+
+
+def predict(input):
+    pred=xgboostModel.predict(input)[0]
+    print(pred)
+    return pred
