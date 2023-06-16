382f1d2bfa1413f323c982b7600c1f6dbdf5b7a2
jack80806 <jack80806@gmail.com>
2023-06-17 03:41:40 +0800

    testForFlag

--- app/__init__.py
+++ app/__init__.py
@@ -6,40 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/basicInfo', methods=['POST'])
-def postForBasicInfo():
-    # 取得前端傳過來的數值
-    insertValues = request.get_json()
-    x1=insertValues['userID']
-    flag = 1
-    result = {
-    "flag": flag,
-    "result":
-    {
-        "family history": "心臟病, 高血壓, 糖尿病",
-        "weight": "60",
-        "age": "22",
-        "height": "170"
-    }
 
-}     # LINE API 
-    return jsonify(result)
-
-@app.route('/message', methods=['POST'])
-def postForMessage():
-    insertValues = request.get_json()
-    x1=insertValues['userID']
-    flag = 1
-    message = "我今天頭很痛" # LINE API 症狀
-    result = 
-    {
-        "flag": flag,
-        "result":
-        {
-            "message": message
-        }
-    }
-    return jsonify(result)
 
 
 @app.route('/test', methods=['GET'])
@@ -69,6 +36,7 @@ def getResult3():
     
     #['Ace': {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}]
     result = {
+    "flag":1,    
     "result": [
         {
             "id": "Ace",
