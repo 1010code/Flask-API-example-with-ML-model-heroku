dd9c916be2e3819c4265f5f3e78dfb54eda1a645
jack80806 <jack80806@gmail.com>
2023-06-17 03:35:26 +0800

    basicInfo&message

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,44 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/userInfo', methods=['GET'])
-def getUserInfo():
+@app.route('/basicInfo', methods=['POST'])
+def postForBasicInfo():
+    # 取得前端傳過來的數值
+    insertValues = request.get_json()
+    x1=insertValues['userID']
+    flag = 1
+    result = {
+    "flag": flag,
+    "result":
+    {
+        "family history": "心臟病, 高血壓, 糖尿病",
+        "weight": "60",
+        "age": "22",
+        "height": "170"
+    }
+
+}     # LINE API 
+    return jsonify(result)
+
+@app.route('/message', methods=['POST'])
+def postForMessage():
+    insertValues = request.get_json()
+    x1=insertValues['userID']
+    flag = 1
+    message = "我今天頭很痛" # LINE API 症狀
+    result = 
+    {
+        "flag": flag,
+        "result":
+        {
+            "message": message
+        }
+    }
+    return jsonify(result)
+
+
+@app.route('/test', methods=['GET'])
+def test():
     
     #['Ace': {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}]
     result = {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}
@@ -15,10 +51,8 @@ def getUserInfo():
 
 @app.route('/right', methods=['POST'])
 def postInput():
-    # 取得前端傳過來的數值
     insertValues = request.get_json()
     x1=insertValues['userID']
-    # 進行預測
     result = 1
     text = "我今天頭很痛" # LINE API 症狀
     return jsonify({'result': str(result), 'text': str(text)})
