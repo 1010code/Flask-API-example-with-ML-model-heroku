6041b7ad65acdb18d7f7c7870009b9c8908563e0
jack80806 <jack80806@gmail.com>
2023-06-16 21:14:12 +0800

    result

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -9,16 +8,9 @@ CORS(app)
 
 @app.route('/test', methods=['GET'])
 def getResult():
-    result = '[
-        {
-            "id": "Ace",
-            "family history": "心臟病, 高血壓, 糖尿病",
-            "weight": "60",
-            "age": "22",
-            "height": "170"
-        }
-    ]'
-    return jsonify({'result': str(result)})
+    result = {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}
+
+    return jsonify(result)
 
 @app.route('/predict', methods=['POST'])
 def postInput():
@@ -26,5 +18,5 @@ def postInput():
     insertValues = request.get_json()
     x1=insertValues['userID']
     # 進行預測
-    result = x1+1
+    result = '我起床的時候頭暈'
     return jsonify({'result': str(result)})
