348cfc70d0f5da6698ee49e01bb091d882e877a2
jack80806 <jack80806@gmail.com>
2023-06-17 03:08:42 +0800

    forTest3

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/test', methods=['GET'])
-def getResult():
+@app.route('/userInfo', methods=['GET'])
+def getUserInfo():
     
     #['Ace': {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}]
     result = {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}
@@ -29,3 +29,20 @@ def getResult2():
     #['Ace': {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}]
     result = 1
     return jsonify({'result': str(result)})
+
+@app.route('/test3', methods=['GET'])
+def getResult3():
+    
+    #['Ace': {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}]
+    result = {
+    "result": [
+        {
+            "id": "Ace",
+            "family history": "心臟病, 高血壓, 糖尿病",
+            "weight": "60",
+            "age": "22",
+            "height": "170"
+        }
+    ]
+}
+    return jsonify(result)
