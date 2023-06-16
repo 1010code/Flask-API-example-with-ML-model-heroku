082a6ba8ac3f14da3a34791e0bc18872d3a83e80
jack80806 <jack80806@gmail.com>
2023-06-16 22:59:14 +0800

    result =1

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,9 @@ CORS(app)
 @app.route('/test', methods=['GET'])
 def getResult():
 
-    #result = {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}
+    #['Ace': {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}]
     result = 1
-    return jsonify(str(result))
+    return jsonify('result':str(result))
 
 @app.route('/right', methods=['POST'])
 def postInput():
