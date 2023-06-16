a71f8e32c4ee3e006c0a0d8c2b387b3b8fdacd90
jack80806 <jack80806@gmail.com>
2023-06-16 23:02:45 +0800

    {}

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ def getResult():
 
     #['Ace': {"id": "Ace","family history": "心臟病, 高血壓, 糖尿病","weight": "60","age": "22","height": "170"}]
     result = 1
-    return jsonify('result':str(result))
+    return jsonify({'result':str(result)})
 
 @app.route('/right', methods=['POST'])
 def postInput():
