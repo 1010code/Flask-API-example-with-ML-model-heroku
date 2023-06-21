5ee6e44d042d7060df5b50534db9ed3c113f582b
jack80806 <jack80806@gmail.com>
2023-06-22 01:45:37 +0800

    del flag

--- src/main.py
+++ src/main.py
@@ -8,85 +8,9 @@ import time
 
 app = FastAPI()
 
-# Global variables
-flagForSymptoms: Optional[int] = 0
-flagForBasicInfo: Optional[int] = 0
-flagForRecords: Optional[int] = 1
-flagForClinic: Optional[int] = 1
-
-urlForChange= "https://for-api-32f276cf322d.herokuapp.com/changeFlag"
-
-class FlagModel(BaseModel):
-    target: str
-    val: int
-   
-
-
-@app.post("/changeFlag")
-def post_for_change_flag(flag: FlagModel):
-    global flagForSymptoms
-    global flagForBasicInfo
-    global flagForRecords
-    global flagForClinic
-
-    if flag.target == "flagForSymptoms":
-        flagForSymptoms = flag.val
-        return "OK"
-    
-    elif flag.target == "flagForBasicInfo":
-        flagForBasicInfo = flag.val
-        return "OK"
-    
-    elif flag.target == "flagForRecords":
-        flagForRecords = flag.val
-        return "OK"
-    
-    elif flag.target == "flagForClinic":
-        flagForClinic = flag.val
-        return "OK"
-    
-
-    return "fail"
-
-
-
-class AllFlagModel(BaseModel):
-    flagForSymptoms: int
-    flagForBasicInfo: int
-    flagForRecords: int
-    flagForClinic: int
-
-
-@app.post("/changeAllFlag")
-def post_for_change_All_flag(flag: AllFlagModel):
-    global flagForSymptoms
-    global flagForBasicInfo
-    global flagForRecords
-    global flagForClinic
-
-    flagForSymptoms = flag.flagForSymptoms
-    flagForBasicInfo = flag.flagForBasicInfo
-    flagForRecords = flag.flagForRecords
-    flagForClinic = flag.flagForClinic
-
-    return "OK"
-
-
-@app.get("/getFlag")
-def get_for_flag():
-    return jsonable_encoder({
-        "flagForSymptoms": flagForSymptoms,
-        "flagForBasicInfo": flagForBasicInfo,
-        "flagForRecords": flagForRecords,
-        "flagForClinic": flagForClinic,
-
-    })
-
-
 @app.get("/basicInfo")
 def get_for_basic_info():
     userid = "Ue1350bef1851afd418a9aa81e444eaa7"
-    flag = flagForBasicInfo
 
     # Prepare the data
     data = {'userid': userid}
@@ -101,23 +25,10 @@ def get_for_basic_info():
 
     # Prepare the final result
     result = {
-        "flag": flag,
         "userID":userid,
         "result": user_data['result']
     }
-    # changeFlag = {
-    #     "target": "flagForBasicInfo",
-    #     "val": 0 
-    # }
-    # for i in range(5):
-    # # Send a POST request
-    #     response = requests.post(urlForChange, json=changeFlag)
-    #     # Extract data from the response
-    #     if response.status_code == 200:
-    #         user_data = response.json()
-    #     else:
-    #         user_data = {}
-    #     time.sleep(0.2)
+
             
     return jsonable_encoder(result)
 
@@ -190,7 +101,6 @@ class ClinicModel(BaseModel):
 @app.post("/forClinic")
 def post_for_clinic(clinic: ClinicModel):
     userid = clinic.userID
-    flag = flagForClinic
     # Prepare the data
     data = {'userid': userid}
     # Send a POST request
@@ -216,7 +126,6 @@ class RecordsModel(BaseModel):
 @app.post("/records")
 def post_for_records(records: RecordsModel):
     userid = records.userID
-    flag = flagForRecords
 
     # Prepare the data
     data = {'userid': userid}
